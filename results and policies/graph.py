66aa0596a63b6aa49b4b772dbe5665d76389d4cb
SteiniSaem <thorsteinns16@ru.is>
2018-11-01 21:58:06 +0000

    added graph.py

---
+++ results and policies/graph.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+with open("FINAL MC_Results.txt") as f:
+    numbers = [x.strip().split(':') for x in f.readlines()]
+
+
+sum = 0
+averages = []
+dividers = []
+divider = 10000
+count = 0
+for i in numbers:
+	i[0] = int(i[0])
+	i[1] = float(i[1])
+	if i[0] >= divider:
+		averages.append(sum/count)
+		sum = 0
+		count = 0
+		dividers.append(divider)
+		divider += 10000
+	else:
+		sum += i[1]
+		count += 1
+
+plt.plot(averages)
+plt.show()
